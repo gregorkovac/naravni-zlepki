399e7569e3a12cc25af502e8d09452b31a831061
Gregor Kovač <79193306+gregorkovac@users.noreply.github.com>
2024-03-28 18:57:39 +0100

    Interpolation works

--- zlepek.py
+++ zlepek.py
@@ -2,22 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Polinom:
-    def __init__(self, a, b, c, d):
-        self.a = a
-        self.b = b
-        self.c = c
-        self.d = d
+    def __init__(self, a_0, a_1, a_2, a_3, x1):
+        self.a_0 = a_0
+        self.a_1 = a_1
+        self.a_2 = a_2
+        self.a_3 = a_3
+        self.x1 = x1
 
     def v(self, x):
-        return self.a * x**3 + self.b * x**2 + self.c * x + self.d
+        return self.a_0 + self.a_1 * (x - self.x1) + self.a_2 * (x - self.x1) ** 2 + self.a_3 * (x - self.x1) ** 3
 
 class Zlepek:
-    interpolacijske_tocke = []
-    polinomi = []
-
     def __init__(self, x, y):
         self.interpolacijske_tocke = np.array([x, y])
+        self.polinomi = []
 
+def f(x):
+    return x ** 4
+
+def dfdx(x):
+    return 4 * x ** 3
 
 def interpoliraj(x, y):
     Z = Zlepek(x, y)
@@ -28,12 +32,13 @@ def interpoliraj(x, y):
         y1 = y[i]
         y2 = y[i + 1]
 
-        a = 0
-        b = 0
-        c = (y2 - y1) / (x2 - x1)
-        d = y1 - c * x1
+        h = x2 - x1
+        a_0 = y1
+        a_1 = dfdx(x1)
+        a_2 = 3 / h ** 2 * (y2 - y1) - 1 / h * (dfdx(x2) + 2 * dfdx(x1))
+        a_3 = 2/h ** 3 * (y1 - y2) + 1/h ** 2 * (dfdx(x1) + dfdx(x2))
 
-        Z.polinomi.append(Polinom(a, b, c, d))
+        Z.polinomi.append(Polinom(a_0, a_1, a_2, a_3, x1))
 
     return Z
 
@@ -54,11 +59,12 @@ def plot(Z):
         plt.plot(x_p, y_p, col)
 
     plt.plot(x, y, 'ko')
+
     plt.show()
 
 def main():
-    x = [0, 1, 2, 3, 4]
-    y = [0, 1.5, 2, 1, 3]
+    x = np.array([-2, -1, 0, 1, 2])
+    y = f(x)
 
     Z = interpoliraj(x, y)
     plot(Z)
