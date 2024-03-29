8ee9f3576e64b49d33e6f53e6ff0130a1f59136c
Gregor Kovač <79193306+gregorkovac@users.noreply.github.com>
2024-03-29 09:39:37 +0100

    Fixed version

--- zlepek.py
+++ zlepek.py
@@ -2,43 +2,73 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class Polinom:
-    def __init__(self, a_0, a_1, a_2, a_3, x1):
+    def __init__(self, a_0, a_1, a_2, a_3):
         self.a_0 = a_0
         self.a_1 = a_1
         self.a_2 = a_2
         self.a_3 = a_3
-        self.x1 = x1
 
     def v(self, x):
-        return self.a_0 + self.a_1 * (x - self.x1) + self.a_2 * (x - self.x1) ** 2 + self.a_3 * (x - self.x1) ** 3
+        return self.a_0 + self.a_1 * x + self.a_2 * x ** 2 + self.a_3 * x ** 3
 
 class Zlepek:
     def __init__(self, x, y):
         self.interpolacijske_tocke = np.array([x, y])
         self.polinomi = []
 
-def f(x):
-    return x ** 4
+    def coefs(self, x, i):
+        return [0] * 4 * i + [1, x, x ** 2, x ** 3] + [0] * (4 * (self.interpolacijske_tocke.shape[1] - i - 2))
+    
+    def coefs_prime(self, x, i):
+        return [0] * 4 * (i - 1) + [0, 1, 2 * x, 3 * x ** 2, 0, -1, -2 * x, -3 * x ** 2] + [0] * (4 * (self.interpolacijske_tocke.shape[1] - i - 2))
 
-def dfdx(x):
-    return 4 * x ** 3
+    def coefs_double_prime(self, x, i):
+        return [0] * 4 * (i - 1) + [0, 0, 2, 6 * x, 0, 0, -2, -6 * x] + [0] * (4 * (self.interpolacijske_tocke.shape[1] - i - 2))
+
+    def coefs_natural(self, x, i):
+        return [0] * 4 * i + [0, 0, 2, 6 * x] + [0] * (4 * (self.interpolacijske_tocke.shape[1] - i - 2))
 
 def interpoliraj(x, y):
     Z = Zlepek(x, y)
 
+    A = []
+    b = []
+
     for i in range(len(x) - 1):
+
         x1 = x[i]
         x2 = x[i + 1]
         y1 = y[i]
         y2 = y[i + 1]
 
-        h = x2 - x1
-        a_0 = y1
-        a_1 = dfdx(x1)
-        a_2 = 3 / h ** 2 * (y2 - y1) - 1 / h * (dfdx(x2) + 2 * dfdx(x1))
-        a_3 = 2/h ** 3 * (y1 - y2) + 1/h ** 2 * (dfdx(x1) + dfdx(x2))
+        A.append(Z.coefs(x1, i))
+        b.append(y1)
 
-        Z.polinomi.append(Polinom(a_0, a_1, a_2, a_3, x1))
+        A.append(Z.coefs(x2, i))
+        b.append(y2)
+
+        if i > 0:
+            A.append(Z.coefs_prime(x1, i))
+            b.append(0)
+
+            A.append(Z.coefs_double_prime(x1, i))
+            b.append(0)
+        
+        if i == 0:
+            A.append(Z.coefs_natural(x1, i))
+            b.append(0)
+        elif i == len(x) - 2:
+            A.append(Z.coefs_natural(x2, i))
+            b.append(0)
+
+
+    A = np.array(A)
+    b = np.array(b)
+
+    x = np.linalg.solve(A, b)
+
+    for i in range(len(x) // 4):
+        Z.polinomi.append(Polinom(x[4 * i], x[4 * i + 1], x[4 * i + 2], x[4 * i + 3]))
 
     return Z
 
@@ -64,7 +94,7 @@ def plot(Z):
 
 def main():
     x = np.array([-2, -1, 0, 1, 2])
-    y = f(x)
+    y = x ** 4
 
     Z = interpoliraj(x, y)
     plot(Z)
